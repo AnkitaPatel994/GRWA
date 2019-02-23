3357bc67947f4ee9322b16ebb73a044f29cd812b
MaitriBhavsar <maitribhavsar23@gmail.com>
2019-02-23 12:03:10 +0530

    view Profile Activity

--- app/src/main/java/com/iteration/grwa/ProfileActivity.java
+++ app/src/main/java/com/iteration/grwa/ProfileActivity.java
@@ -1,13 +1,245 @@
 package com.iteration.grwa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    CircleImageView ivUserProfilePhoto;
+    TextView txtProfileName,txtProfileEmail,txtProfileMobile;
+    SessionManager session;
+    String user_id;
+    Dialog dialog;
+    EditText txtProfilePhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ivUserProfilePhoto = (CircleImageView)findViewById(R.id.ivUserProfilePhoto);
+        txtProfileName = (TextView)findViewById(R.id.txtProfileName);
+        txtProfileEmail = (TextView)findViewById(R.id.txtProfileEmail);
+        txtProfileMobile = (TextView)findViewById(R.id.txtProfileMobile);
+
+        session = new SessionManager(getApplicationContext());
+        HashMap<String,String> user = session.getUserDetails();
+        user_id = user.get(SessionManager.user_id);
+
+        GetViewProfile viewProfile = new GetViewProfile();
+        viewProfile.execute();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.menu_profile_edit) {
+
+            dialog = new Dialog(ProfileActivity.this,android.R.style.Theme_Light_NoTitleBar);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+            dialog.setContentView(R.layout.profile_edit_dialog);
+            dialog.setCancelable(true);
+
+            txtProfilePhone=(EditText) dialog.findViewById(R.id.txtProfilePhone);
+
+            Button btnProfileSubmit=(Button) dialog.findViewById(R.id.btnProfileSubmit);
+            btnProfileSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    GetEditProfileMobile editProfileMobile = new GetEditProfileMobile();
+                    editProfileMobile.execute();
+                }
+            });
+
+            dialog.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        /*if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private class GetViewProfile extends AsyncTask<String,Void,String> {
+
+        String status,message,e_id,e_name,ProfilepImg,e_email,e_mobile;
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            JSONObject joUser=new JSONObject();
+            try {
+                joUser.put("UserId",user_id);
+                Postdata postdata = new Postdata();
+                String pdUser=postdata.post(MainActivity.BASE_URL+"ViewEmployee.php",joUser.toString());
+                JSONObject j = new JSONObject(pdUser);
+                status=j.getString("status");
+                if(status.equals("1"))
+                {
+                    message=j.getString("message");
+                    JSONArray JsArry=j.getJSONArray("Employee");
+                    for (int i=0;i<JsArry.length();i++)
+                    {
+                        JSONObject jo=JsArry.getJSONObject(i);
+
+                        e_id =jo.getString("e_id");
+                        e_name =jo.getString("e_name");
+                        String e_pic =jo.getString("e_pic");
+                        ProfilepImg =MainActivity.BASE_URL+e_pic;
+                        e_email =jo.getString("e_email");
+                        e_mobile =jo.getString("e_mobile");
+
+
+                    }
+                }
+                else
+                {
+                    message=j.getString("message");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(status.equals("1"))
+            {
+                txtProfileName.setText(e_name);
+                txtProfileEmail.setText(e_email);
+                txtProfileMobile.setText("+91 "+e_mobile);
+                Picasso.with(ProfileActivity.this).load(ProfilepImg).into(ivUserProfilePhoto);
+            }
+            else
+            {
+                Toast.makeText(ProfileActivity.this,message,Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class GetEditProfileMobile extends AsyncTask<String,Void,String> {
+
+        String status,message;
+        @Override
+        protected String doInBackground(String... strings) {
+
+            JSONObject joUser=new JSONObject();
+            try {
+                joUser.put("e_id",user_id);
+                joUser.put("e_mobile",txtProfilePhone);
+                Postdata postdata = new Postdata();
+                String pdUser=postdata.post(MainActivity.BASE_URL+"editEmploy.php",joUser.toString());
+                JSONObject j = new JSONObject(pdUser);
+                status=j.getString("status");
+                if(status.equals("1"))
+                {
+                    message=j.getString("message");
+                }
+                else
+                {
+                    message=j.getString("message");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(status.equals("1"))
+            {
+                dialog.dismiss();
+                Toast.makeText(ProfileActivity.this,message,Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(ProfileActivity.this,message,Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
